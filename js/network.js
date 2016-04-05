7068dafbe0d4ffb37f5163fcab0aff8e42f254f9
rob <rob@codebox.org.uk>
2016-04-05 15:03:24 +0100

    refactor, better test cases

--- js/network.js
+++ js/network.js
@@ -1,31 +1,39 @@
-
-// see http://cs231n.github.io/convolutional-networks/#pool
 function buildNetwork(inputLayer) {
     "use strict";
 
-    function buildLayer(w, h, d) {
+    var layers = [];
+
+    function buildLayer(w, h, d, weights) {
         return {
-            w : w,
-            h : h,
-            d : d,
-            size : function () {
-                return this.w * this.h * this.d;
-            }
+            w: w,
+            h: h,
+            d: d,
+            weights : weights
         };
     }
 
-    var layers = [buildLayer(inputLayer.w, inputLayer.h, inputLayer.d)];
-
-    function isNotInt(n) {
-        return Math.floor(n) !== n;
+    function addLayer(w, h, d, weights) {
+        layers.push(buildLayer(w, h, d, weights));
     }
 
     function getPreviousLayer() {
         return layers[layers.length - 1];
     }
 
+    function isNotInt(n) {
+        return Math.floor(n) !== n;
+    }
+
     return {
-        addConvolution : function (patchWidth, patchHeight, strideWidth, strideHeight, zeroPaddingWidth, zeroPaddingHeight, outputCount) {
+        withInputLayer : function(w, h, d) {
+            if (layers.length) {
+                throw new Error('Input layer must be the first layer to be added');
+            }
+            addLayer(w, h, d, 0);
+            return this;
+        },
+
+        withConvLayer : function(patchWidth, patchHeight, strideWidth, strideHeight, zeroPaddingWidth, zeroPaddingHeight, outputCount) {
             var previousLayer = getPreviousLayer(),
                 wn = (previousLayer.w - patchWidth + 2 * zeroPaddingWidth),
                 w  = 1 + wn / strideWidth,
@@ -41,20 +49,20 @@ function buildNetwork(inputLayer) {
                 throw new Error(["Bad strideHeight value:", strideHeight, "is not a factor of", hn].join(' '));
             }
 
-            layers.push(buildLayer(h, w, d));
+            addLayer(w, h, d, previousLayer.d * patchWidth * patchHeight * outputCount);
+
+            return this;
         },
 
-        addConvolutionSimple : function (patchSize, stride, zeroPaddingSize, outputCount) {
-            this.addConvolution(patchSize, patchSize, stride, stride, zeroPaddingSize, zeroPaddingSize, outputCount);
-        },
-
-        addRelu : function () {
+        withRelu : function(){
             var previousLayer = getPreviousLayer();
 
-            layers.push(buildLayer(previousLayer.h, previousLayer.w, previousLayer.d));
+            addLayer(previousLayer.w, previousLayer.h, previousLayer.d, 0);
+
+            return this;
         },
 
-        addPooling : function (poolWidth, poolHeight, strideWidth, strideHeight) {
+        withPooling : function(poolHeight, poolWidth, strideWidth, strideHeight) {
             var previousLayer = getPreviousLayer(),
                 wn = (previousLayer.w - poolWidth),
                 w  = 1 + wn / strideWidth,
@@ -69,31 +77,26 @@ function buildNetwork(inputLayer) {
                 throw new Error(["Bad strideHeight value:", strideHeight, "must be a factor of", hn].join(' '));
             }
 
-            layers.push(buildLayer(h, w, previousLayer.d));
+            addLayer(h, w, previousLayer.d, 0);
+
+            return this;
         },
 
-        addPoolingSimple : function (size) {
-            this.addPooling(size, size, size, size);
+        withFullyConnectedLayer : function(w, h, d) {
+            var previousLayer = getPreviousLayer();
+
+            addLayer(w, h, d, previousLayer.w * previousLayer.h * previousLayer.d * w * h * d);
+
+            return this;
         },
 
-        addFullyConnected : function (size) {
-            layers.push(buildLayer(1, 1, size));
+        withOutputLayer : function(classCount) {
+            return this.withFullyConnectedLayer(1, 1, classCount);
         },
 
         getLayers : function() {
-            var previousLayer;
             return layers.map(function(layer){
-                var populatedLayer = buildLayer(layer.w, layer.h, layer.d);
-
-                if (previousLayer) {
-                    populatedLayer.i = previousLayer.o;
-                } else {
-                    populatedLayer.i = layer.size();
-                }
-
-                populatedLayer.o = layer.size();
-                previousLayer = populatedLayer;
-                return populatedLayer;
+                return buildLayer(layer.w, layer.h, layer.d, layer.weights);
             });
         }
     };
--- js/tests/network-tests.js
+++ js/tests/network-tests.js
@@ -1,5 +1,12 @@
 describe("network", function() {
-    var net, inputLayer;
+    var net;
+
+    function checkLayer(expectedLayer, actualLayer) {
+        expect(actualLayer.h).toEqual(expectedLayer.h);
+        expect(actualLayer.w).toEqual(expectedLayer.w);
+        expect(actualLayer.d).toEqual(expectedLayer.d);
+        expect(actualLayer.weights).toEqual(expectedLayer.weights);
+    }
 
     function checkNetwork(expectedLayers) {
         var actualLayers = net.getLayers();
@@ -7,69 +14,91 @@ describe("network", function() {
         expect(actualLayers.length).toBe(expectedLayers.length);
 
         actualLayers.forEach(function(actualLayer, i) {
-            var expectedLayer = expectedLayers[i];
-
-            expect(actualLayer.h).toEqual(expectedLayer.h);
-            expect(actualLayer.w).toEqual(expectedLayer.w);
-            expect(actualLayer.d).toEqual(expectedLayer.d);
-            expect(actualLayer.i).toEqual(expectedLayer.i);
-            expect(actualLayer.o).toEqual(expectedLayer.o);
+            checkLayer(expectedLayers[i], actualLayer)
         });
     }
 
-    function buildLayer(w, h, d) {
-        return {
-            w : w,
-            h : h,
-            d : d
-        };
-    }
-
-    it("input layer only", function() {
-        net = buildNetwork(buildLayer(16, 16, 3));
-
-        checkNetwork([{h:16, w:16, d:3, i:768, o:768}]);
+    beforeEach(function(){
+        net = buildNetwork();
     });
 
-    it("simple convolution stride 1", function() {
-        net = buildNetwork(buildLayer(16, 16, 3));
-
-        net.addConvolutionSimple(3, 1, 0, 2);
+    it("linear classifier", function() {
+        net.withInputLayer(100, 100, 1)
+            .withOutputLayer(10);
 
         checkNetwork([
-            {h:16, w:16, d:3, i:768, o:768},
-            {h:14, w:14, d:2, i:768, o:392}
+            {h:100, w:100, d:1,  weights:0},
+            {h:1,   w:1,   d:10, weights:100*100*10}
         ]);
+
     });
 
-    it("simple convolution stride 2", function() {
-        net = buildNetwork(buildLayer(17, 17, 1));
-
-        net.addConvolutionSimple(5, 2, 0, 4);
+    it("conv net", function() {
+        net.withInputLayer(128, 128, 3)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 6)
+            .withRelu()
+            .withPooling(2, 2, 2, 2)
+            .withFullyConnectedLayer(32, 32, 6)
+            .withOutputLayer(10);
 
         checkNetwork([
-            {h:17, w:17, d:1, i:289, o:289},
-            {h: 7, w: 7, d:4, i:289, o:196}
+            {h:128, w:128, d:3,  weights:0},
+            {h:128, w:128, d:6,  weights:3*3*3*6},
+            {h:128, w:128, d:6,  weights:0},
+            {h:64,  w:64,  d:6,  weights:0},
+            {h:32,  w:32,  d:6,  weights:64*64*6*32*32*6},
+            {h:1,   w:1,   d:10, weights:32*32*6*10}
         ]);
+
     });
-
-    it("simple convolution stride 3 with padding", function() {
-        net = buildNetwork(buildLayer(14, 14, 1));
-
-        net.addConvolutionSimple(7, 3, 1, 2);
+    it("VGGNet", function() {
+        // validated against http://cs231n.github.io/convolutional-networks/#case
+        net.withInputLayer(224, 224, 3)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 64)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 64)
+            .withPooling(2, 2, 2, 2)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 128)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 128)
+            .withPooling(2, 2, 2, 2)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 256)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 256)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 256)
+            .withPooling(2, 2, 2, 2)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 512)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 512)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 512)
+            .withPooling(2, 2, 2, 2)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 512)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 512)
+            .withConvLayer(3, 3, 1, 1, 1, 1, 512)
+            .withPooling(2, 2, 2, 2)
+            .withFullyConnectedLayer(1, 1, 4096)
+            .withFullyConnectedLayer(1, 1, 4096)
+            .withOutputLayer(1000);
 
         checkNetwork([
-            {h:14, w:14, d:1, i:196, o:196},
-            {h: 4, w: 4, d:2, i:196, o:32}
+            {h:224, w:224, d:3,   weights:0},
+            {h:224, w:224, d:64,  weights:(3*3*3)*64},
+            {h:224, w:224, d:64,  weights:(3*3*64)*64},
+            {h:112, w:112, d:64,  weights:0},
+            {h:112, w:112, d:128, weights:(3*3*64)*128},
+            {h:112, w:112, d:128, weights:(3*3*128)*128},
+            {h:56,  w:56,  d:128, weights:0},
+            {h:56,  w:56,  d:256, weights:(3*3*128)*256},
+            {h:56,  w:56,  d:256, weights:(3*3*256)*256},
+            {h:56,  w:56,  d:256, weights:(3*3*256)*256},
+            {h:28,  w:28,  d:256, weights:0},
+            {h:28,  w:28,  d:512, weights:(3*3*256)*512},
+            {h:28,  w:28,  d:512, weights:(3*3*512)*512},
+            {h:28,  w:28,  d:512, weights:(3*3*512)*512},
+            {h:14,  w:14,  d:512, weights:0},
+            {h:14, w:14,  d:512,  weights:(3*3*512)*512},
+            {h:14, w:14,  d:512,  weights:(3*3*512)*512},
+            {h:14, w:14,  d:512,  weights:(3*3*512)*512},
+            {h:7,  w:7,   d:512,  weights:0},
+            {h:1, w:1, d:4096,    weights:7*7*512*4096},
+            {h:1, w:1, d:4096,    weights:4096*4096},
+            {h:1, w:1, d:1000,    weights:4096 * 1000}
         ]);
     });
-
-    it("simple convolution with bad stride value", function() {
-        net = buildNetwork(buildLayer(14, 14, 1));
-
-        expect(function() {
-            net.addConvolutionSimple(3,2,0,2);
-        }).toThrow(new Error("Bad strideWidth value: 2 is not a factor of 11"));
-    });
-
 });
